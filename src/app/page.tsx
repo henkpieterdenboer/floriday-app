13c4e4284483f47f423871debc0b599598dafe95
Henk Pieter den Boer <henkpieterdenboer@gmail.com>
2026-08-02 14:09:46 +0200

    feat: send the home page straight to the supply overview

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,12 @@
+import { redirect } from "next/navigation";
+
+/**
+ * De hoofdpagina bestaat niet als zelfstandig scherm; de applicatie is het aanbodoverzicht.
+ *
+ * Doorsturen naar /aanbod in plaats van naar /login, want de middleware beschermt dat pad
+ * al: wie niet is aangemeld belandt vanzelf op /login?verder=/aanbod en komt na aanmelden
+ * meteen op de goede plek uit. Rechtstreeks naar /login sturen zou die terugweg weggooien.
+ */
 export default function Home() {
-  return <main>Floriday middleware</main>;
+  redirect("/aanbod");
 }
